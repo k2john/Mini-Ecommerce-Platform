7f7dd7a5f078412f2edde112ea9af889e719936f
John L. Menguis <johnlmenguis@gmail.com>
2026-04-29 18:44:59 +0800

    Updated backend API and frontend and fixed bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,10 +3,22 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     host: true,
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
   },
   preview: {
     host: true,
-    port: 4173
+    port: 4173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true
+      }
+    }
   }
 });
